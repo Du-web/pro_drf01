59ff9b6a24a41245afca8af57632da852088b969
dudu <648302938@qq.com>
2020-09-10 17:06:45 +0800

    在配置文件中挂载drfapp,并在app中创建urls.py文件，配置路由

--- pro_day01/urls.py
+++ pro_day01/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('userapp/', include('userapp.urls')),
+    path('drfapp/', include('drfapp.urls')),
+
 ]
